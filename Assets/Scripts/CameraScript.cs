5f44c2917a6bc3879fd0899aef15df4dabd0cb24
Steven Alford <steven.alford@collinsonlatitude.com>
2016-01-30 23:51:50 +0800

    Draw turn camera.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,12 +1,29 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class CameraScript : MonoBehaviour
 {
-	void Start() { }
+    private float yawSpeed;
+    private float pitchSpeed;
+    private Vector3 lastMousePos;
 
-	void Update()
-	{
-		transform.Rotate(Vector3.up, Time.deltaTime * 2);
-	}
+	private void Start() { }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            yawSpeed = -45f*Input.GetAxis("Mouse X")*Time.deltaTime;
+            pitchSpeed = 45f*Input.GetAxis("Mouse Y")*Time.deltaTime;
+        }
+        else
+        {
+            yawSpeed = 0f;
+            pitchSpeed = 0f;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        transform.rotation *= Quaternion.Euler(pitchSpeed, yawSpeed, 0f);
+    }
 }
